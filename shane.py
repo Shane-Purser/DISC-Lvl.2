d5b75f9c3de0ecfe491430afbead96ce0da79a15
pursers <pursers@stu.otc.school.nz>
2022-08-05 09:30:36 +1200

    I tidied up the code and also added the number_of_dice variable so you could change how many dice you were rolling. Also added some comments so I knew and others who read my code will know what the code is for.

--- shane.py
+++ shane.py
@@ -1,16 +1,20 @@
 import random
 
-def roll_dice():
-    side_of_die = input("Are you using three sided dice or six \n For a 3 sided die enter: Three \n For a 6 sided die enter: Six \n Enter:")
-    capatilise_string = side_of_die.capitalize()
+dice_sides = eval(input("Please enter in numbers e.g. 3, 6 \n How many sides to the dice:"))     # These two lines are used to make the roll_dice function look tidier when it is called on
+dice_number = eval(input("Please enter in numbers e.g. 1, 2, 3 \n How many dice: "))
 
 
-    if capatilise_string == "Three":
-        n = random.randint(1,3)
-        print(n)
-    elif capatilise_string == "Six":
-        n = random.randit(1,6)
-        print(n)
-    else:
-        print("Please enter a different value")
-roll_dice()
+def roll_dice(side_of_dice, number_of_dice):         # Function that has the code needed to roll certain sided dice a certain number of times
+    while number_of_dice >= 1:                       # While loop controls the amount of time the 3 or 6 sided dice is rolled
+        if side_of_dice == 3:                        # If ,Elif and Else code to pick whether the number entered is valid or if the code should ask for a different input value
+            n = random.randint(1, 3)
+            print(n)
+        elif side_of_dice == 6:
+            n = random.randint(1, 6)
+            print(n)
+        else:
+            print("Please enter a different value")
+        number_of_dice -= 1
+
+
+roll_dice(dice_sides, dice_number)
