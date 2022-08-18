ab04f361a730c30e54fbc49d50db6ca2fa614638
pursers <pursers@stu.otc.school.nz>
2022-08-19 09:20:11 +1200

    I created a new piece of code that runs the Shooting Phase of my game. this is only a small part of the shooting phase and is still unfinished. what the code does is get a troop name and stats from a user and inserts them into lists the lists will be able to be called on later and then all the troops and stats are at your fingertips.

---
+++ shooting_phase_choose_troop.py
@@ -0,0 +1,67 @@
+troop_list = []            # list all weapons will be stored in
+
+t_stats_list = []
+
+t_name_list = []
+
+p = 1
+
+w = 0
+
+while p == 1:
+    print("Troops", t_name_list)
+    troop_location = eval(input("If troop was on list enter: 1 \nIf troop was not on list enter: 2 \nIf you are finished enter: 3\nEnter: "))  # If weapon is in list ask for what weapon it was. If not get a new weapon appended onto the list.
+    if troop_location == 1:
+        print("Troops", t_name_list)
+        troop = eval(input("What troop is being used\ne.g. troop first in list is 1\nTroop: "))
+        print(troop_list[troop - 1], "\nIs this the correct Troop")
+        confirm = eval(input("if this is your troop enter: 1\nIf this is not your troop enter: 2\n enter: "))
+      ##  if confirm == 1:
+            # use the gun get the amount of times it is fired
+        #elif confirm == 2:
+        #    # replay the loop
+       # else:
+        #    print("This is not a valid answer. Please enter a different value.")
+
+    elif troop_location == 2:
+        new_troop = input("What is your Troop called: ")
+        capital_weapon = new_troop.capitalize()
+        t_name_list.append([])                # makes a list in the weapon list then appends the new weapon to the new list.
+        t_name_list[w].append(capital_weapon)
+        troop_ws = eval(input("What is the troops weapon skill:"))                             # weapon strength armour pierce and damage
+        troop_bs = eval(input("What is the troops ballistic skill:"))
+        troop_s = eval(input("What is the troops strength:"))
+        troop_t = eval(input("What is the troops toughness:"))
+        troop_w = eval(input("What is the troops wounds:"))
+        troop_a = eval(input("What is the troops attack:"))
+        troop_ld = eval(input("What is the troops leadership:"))
+        troop_sv = eval(input("What is the troops save:"))
+        troop_iv = eval(input("What is the troops invulnerable(if none enter 0):"))
+        t_stats_list.append([])
+        t_stats_list[w].append(troop_ws)
+        t_stats_list[w].append(troop_bs)
+        t_stats_list[w].append(troop_s)
+        t_stats_list[w].append(troop_t)
+        t_stats_list[w].append(troop_w)
+        t_stats_list[w].append(troop_a)
+        t_stats_list[w].append(troop_ld)
+        t_stats_list[w].append(troop_sv)
+        t_stats_list[w].append(troop_iv)
+        print("Stats", t_stats_list[w])
+        print("Troops", t_name_list, "\n\n")
+        troop_list.append([])
+        troop_list[w].append(capital_weapon)
+        troop_list[w].append(troop_ws)
+        troop_list[w].append(troop_bs)
+        troop_list[w].append(troop_s)
+        troop_list[w].append(troop_t)
+        troop_list[w].append(troop_w)
+        troop_list[w].append(troop_a)
+        troop_list[w].append(troop_ld)
+        troop_list[w].append(troop_sv)
+        troop_list[w].append(troop_iv)
+        w += 1
+    elif troop_location == 3:
+        break
+    else:
+        print("Please enter a different value")
