95daedcd47d22c81f1b4dfd0d5e6f1f9c129342d
pursers <pursers@stu.otc.school.nz>
2022-08-19 09:19:10 +1200

    I created a new piece of code that runs the Shooting Phase of my game. this is only a small part of the shooting phase and is still unfinished. what the code does is get a weapon name and stats from a user and inserts them into lists the lists will be able to be called on later and then all the weapons and stats are at your fingertips. added a bit more to the code so that it would print the gun and stats out of the list depending on what number you enter.

--- shooting_phase_weapon_select.py
+++ shooting_phase_weapon_select.py
@@ -1,21 +1,20 @@
 weapon_list = []            # list all weapons will be stored in
 
-stats_list = []
+w_stats_list = []
 
-name_list = []
+w_name_list = []
 
 p = 1
 
 w = 0
 
 while p == 1:
-    print("Weapons", name_list)
+    print("Weapons", w_name_list)
     weapon_location = eval(input("If weapon was on list enter: 1 \nIf weapon was not on list enter: 2 \nIf you are finished enter: 3\nEnter: "))  # If weapon is in list ask for what weapon it was. If not get a new weapon appended onto the list.
-    print(weapon_location)
     if weapon_location == 1:
-        print("Weapons", name_list)
+        print("Weapons", w_name_list)
         weapon = eval(input("What weapon is being fired\ne.g. weapon first in list is 1\nWeapon: "))
-        print(weapon_list[weapon + 1], "\nIs this the correct weapon\n")
+        print(weapon_list[weapon - 1], "\nIs this the correct weapon\n")
         confirm = eval(input("if this is your weapon enter: 1\nIf this is not your weapon enter: 2\n enter: "))
         if confirm == 1:
             # use the gun get the amount of times it is fired
@@ -27,17 +26,17 @@ while p == 1:
     elif weapon_location == 2:
         new_weapon = input("What is your weapon called: ")
         capital_weapon = new_weapon.capitalize()
-        name_list.append([])                # makes a list in the weapon list then appends the new weapon to the new list.
-        name_list[w].append(capital_weapon)
+        w_name_list.append([])                # makes a list in the weapon list then appends the new weapon to the new list.
+        w_name_list[w].append(capital_weapon)
         weapon_s = eval(input("What is the weapons strength:"))                             # weapon strength armour pierce and damage
         weapon_ap = eval(input("What is the weapons armour pierce:"))
         weapon_d = eval(input("What is the weapons damage:"))
-        stats_list.append([])
-        stats_list[w].append(weapon_s)
-        stats_list[w].append(weapon_ap)
-        stats_list[w].append(weapon_d)
-        print("Stats", stats_list[w])
-        print("Weapons", name_list, "\n\n")
+        w_stats_list.append([])
+        w_stats_list[w].append(weapon_s)
+        w_stats_list[w].append(weapon_ap)
+        w_stats_list[w].append(weapon_d)
+        print("Stats", w_stats_list[w])
+        print("Weapons", w_name_list, "\n\n")
         weapon_list.append([])
         weapon_list[w].append(capital_weapon)
         weapon_list[w].append(weapon_s)
